c5038c3e100e57427390a19b9a1682314f8f78c7
Simon Olofsson <s.olofsson@gmail.com>
2016-06-05 17:18:26 +0200

    Switched back to using jQuery for parallax scroll effects, but tried to fix problem with relative paths.

--- js/mainjQuery.js
+++ js/mainjQuery.js
@@ -1,16 +1,16 @@
 $(function() {
 
-	$('#background').parallax({imageSrc: '/images/background_logo.jpg',
+	$('#background').parallax({imageSrc: 'images/background_logo.jpg',
 							bleed: 100,
 							position: "top"});
 
 
 	$('#members').parallax({
-		imageSrc: '/images/allatre.jpg',
+		imageSrc: 'images/allatre.jpg',
 		bleed: 100,
 		position: "bottom"
 	});
 
-	$('#big-image').parallax({imageSrc: '/images/vidbil.jpg', bleed: 100});
+	$('#big-image').parallax({imageSrc: 'images/vidbil.jpg', bleed: 100});
 
 });
