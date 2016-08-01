36061d1d67128f6ae45070dd6f738bfbf3ed454d
Simon Olofsson <s.olofsson@gmail.com>
2016-08-01 21:15:34 +0200

    AdminNewsController seemingly done. Still not entirely working, though.

--- js/Controllers/AdminNewsController.js
+++ js/Controllers/AdminNewsController.js
@@ -1,11 +1,13 @@
 /**
  * Created by simon on 2016-08-01.
  */
-app.controller('AdminNewsController', function($scope, $rootScope, $http, AuthenticationService, SendObjectService) {
+app.controller('AdminNewsController', function($scope, $rootScope, $http, AuthenticationService, SendObjectService, AppendCredentialsService) {
     var username = localStorage.getItem('username');
     var token = localStorage.getItem('token');
     
-    $scope.getNews = function() {
+    AuthenticationService.checkToken(username, token);
+    
+    function getNews() {
         $http.get($rootScope.serverRoot + 'news').then(function(response) {
             $scope.news = response.data;
         });
@@ -17,29 +19,56 @@ app.controller('AdminNewsController', function($scope, $rootScope, $http, Authen
         content: ''
     };
 
+    $scope.assignFocus = function(newsItem) {
+        $scope.newsItemToBeSent.id = newsItem.id;
+        $scope.newsItemToBeSent.date = newsItem.date;
+        $scope.newsItemToBeSent.content = newsItem.content;
+
+        $scope.heading = 'Redigera nyhet';
+        $scope.newsItemAction = 'Uppdatera nyhet';
+        $scope.addingNewNewsItem = false;
+        $scope.sendNewsItem = $scope.putNewsItem();
+    };
+
+    $scope.removeFocus = function() {
+        angular.forEach($scope.newsItemToBeSent, function(value, key) {
+            $scope.newsItemToBeSent[key] = '';
+        });
+
+        $scope.heading = 'Lägg till nyhet';
+        $scope.newsItemAction = 'Lägg till nyhet';
+        $scope.addingNewNewsItem = true;
+        $scope.sendNewsItem = $scope.postNewsItem;
+    };
+    var newsEndpoint = $rootScope.serverRoot + 'news';
+
     $scope.putNewsItem = function() {
         AppendCredentialsService.appendCredentials($scope.newsItemToBeSent, username, token); 
 
-        SendObjectService.putObject(gigsEndpoint, $scope.gigToBeSent, function(news) {
-            $scope.getNews();
+        SendObjectService.putObject(newsEndpoint, $scope.gigToBeSent, function(news) {
+            getNews();
         });
     };
 
     $scope.postNewsItem = function() {
         AppendCredentialsService.appendCredentials($scope.newsItemToBeSent, username, token); 
 
-        SendObjectService.postObject(gigsEndpoint, $scope.gigToBeSent, function(news) {
-            $scope.getNews();
+        SendObjectService.postObject(newsEndpoint, $scope.gigToBeSent, function(news) {
+            getNews();
         });
     };
 
     $scope.deleteNewsItem = function() {
         AppendCredentialsService.appendCredentials($scope.newsItemToBeSent, username, token); 
 
-        SendObjectService.deleteObject(gigsEndpoint, $scope.gigToBeSent, function(news) {
-            $scope.getNews();
+        SendObjectService.deleteObject(newsEndpoint, $scope.gigToBeSent, function(news) {
+            getNews();
         });
     };
     
-    $scope.getNews();
+    getNews();
+    $scope.heading = 'Lägg till nyhet';
+    $scope.newsItemAction = 'Lägg till nyhet';
+    $scope.addingNewNewsItem = true;
+    $scope.sendNewsItem = $scope.postNewsItem;
 });
