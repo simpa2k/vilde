c17b84dab53e0a04b84bc7b240979607b312af25
Simon Olofsson <s.olofsson@gmail.com>
2016-08-20 19:11:25 +0200

    Removed old sticky-navbar files and fixed some issues related to resizing of the navbar on window resize and navbar toggle.

--- app/modules/vilde/directives/sticky-navbar/sticky-navbar.js
+++ app/modules/vilde/directives/sticky-navbar/sticky-navbar.js
@@ -1,136 +1,140 @@
 define(function() {
 
-    var app = angular.module('vilde');
-    
-    app.directive('stickyNavbar', function() {
-        return {
-            restrict: 'EA',
-            templateUrl: 'app/modules/vilde/directives/sticky-navbar/sticky-navbar.html',
-            link: function($scope, element, attrs) {
-                var navbarTextElements = document.getElementsByClassName('navbar-text');
+   var stickyNavbar = function() {
 
-                function getY(element) {
-                    var boundingClientRect = element.getBoundingClientRect();
-                    return boundingClientRect.top + window.scrollY;
-                }
+       return {
+           restrict: 'E',
+           replace: true,
+           scope: {
+               headings: '='
+           },
+           templateUrl: 'app/modules/vilde/directives/sticky-navbar/sticky-navbar.html',
+           link: function($scope, element, attributes) {
+               $scope.socialMedia = attributes.socialMedia;
 
-                /*
-                 *
-                 * Function to fixate navbar to top of window when scrolling down past it
-                 * and then pop it back when scrolling above it.
-                 *
-                 */
+               var elementToStickTo = $('#' + attributes.elementToStickTo);
+               var collapsedNavbarHeight = element.height();
+               var expandedNavbarHeight;
 
-                var main = document.getElementById('main');
-                var navbar = document.getElementById('navbar');
-                var collapsedNavbarHeight = navbar.getBoundingClientRect().height;
-                var expandedNavbarHeight;
+               var navbarPositionListener = function() {
+                   var navbarHeight = element.hasClass('expanded') ? expandedNavbarHeight : collapsedNavbarHeight;
+                   var elementToStickToY = elementToStickTo.offset().top;
+                   var topOfWindowAlignsWithTopOfHeader = $(window).scrollTop() > (elementToStickToY - navbarHeight);
 
-                function navbarPositionListener() {
-                    var navbarHeight = navbar.classList.contains('expanded') ? expandedNavbarHeight : collapsedNavbarHeight;
-                    var mainY = getY(main);
-                    var topOfWindowAlignsWithTopOfHeader = window.scrollY > (mainY - navbarHeight);
+                   if(topOfWindowAlignsWithTopOfHeader) {
+                       fixateElementPositionTop(element, navbarHeight);
+                   } else {
+                       fixateElementPositionBottom(element, elementToStickToY);
+                   }
 
-                    if (topOfWindowAlignsWithTopOfHeader) {
-                        fixateElementPositionTop(navbar, navbarHeight);
-                    } else {
-                        fixateElementPositionBottom(navbar, mainY);
-                    }
+               };
 
-                }
+               var fixateElementPositionTop = function(element, elementHeight) {
 
-                function fixateElementPositionTop(element, elementHeight) {
+                   element.css({
+                       'z-index': '10',
+                       'position': 'fixed',
+                       'top': '0',
+                       'height': String(elementHeight) + 'px'
+                   });
+               };
 
-                    element.style.zIndex = "10";
-                    element.style.position = "fixed";
-                    element.style.bottom = "";
-                    element.style.top = "0";
-                    element.style.height = String(elementHeight) + "px";
+               var fixateElementPositionBottom = function(element, bottomOffset) {
 
-                }
+                   element.css({
+                       'z-index': '10',
+                       'position': 'absolute',
+                       'bottom': String(bottomOffset),
+                       'top': ''
+                   });
+               };
 
-                function fixateElementPositionBottom(element, bottomOffset) {
+               /*
+                * Sets the enclosing nav element's height to that of the enclosed .container-fluid,
+                * which is what is actually taking the size and padding/margin of the <p>'s or <a>'s into account
+                */
 
-                    element.style.zIndex = "10";
-                    element.style.position = "absolute";
-                    element.style.bottom = String(bottomOffset);
-                    element.style.top = "";
+               var setNavbarHeight = function() {
+                   $(element).css('height', $('#navbar .container-fluid').height() + 'px');
+               };
 
-                }
+               /*
+                * If the navbar is fixed to the top and it's height changes so that it
+                * still is fixed to the top despite it being placed above the element that it should stick to
+                * it has to be fixated to it.
+                */
 
-                window.addEventListener('scroll', navbarPositionListener, false);
+               var checkBottomPosition = function() {
+                   var elementBottom = $(element).offset().top + $(element).height();
 
-                /*
-                 *
-                 * Expands and collapses navbar on small screens.
-                 *
-                 */
+                   if(elementBottom < elementToStickTo.offset().top) {
+                       fixateElementPositionBottom($(element), elementToStickTo.offset().top);
+                   }
+               };
 
-                function toggleNavbar() {
-                    var navbarButton = document.getElementById('navbar-button');
+               /*
+                * If the navbar height is not constantly checked on resize
+                * the .container-fluid might get bigger than the navbar, resulting in ugliness.
+                * The position of the navbar might also change if it gets smaller, resulting in it
+                * being placed above the element that it should stick to.
+                */
 
-                    if(navbar.classList.contains('expanded')) {
-                        navbar.classList.remove('expanded');
-                        navbar.style.height = String(collapsedNavbarHeight) + 'px';
+               var resizeHandler = function() {
+                   setNavbarHeight();
+                   checkBottomPosition();
+               };
 
-                        navbarButton.classList.remove('active');
-                    } else {
-                        navbar.classList.add('expanded');
-                        expandedNavbarHeight = document.querySelector('#navbar .container-fluid').getBoundingClientRect().height;
-                        navbar.style.height = String(expandedNavbarHeight) + 'px';
+               $(window).resize(resizeHandler);
 
-                        var navbarY = getY(navbar);
-                        if(navbarY < window.scrollY) {
-                            window.scrollTo(0, navbarY);
-                        }
 
-                        navbarButton.classList.add('active');
-                    }
+               $scope.goToSection = function(sectionId) {
+                   $('html').animate({
+                       scrollTop: $(sectionId).offset().top
+                   });
+               };
 
-                }
+               $scope.toggleNavbar = function() {
+                   var navbarButton = $('#navbar-button');
 
-                var navbarButton = document.getElementById('navbar-button');
-                navbarButton.addEventListener('click', toggleNavbar, false);
+                   if(element.hasClass('expanded')) {
+                       element.removeClass('expanded');
+                       element.css('height', String(collapsedNavbarHeight) + 'px');
 
-                function headingClick(e) {
-                    var item = e.target;
+                       /*
+                        * If the navbar is collapsed it might end up having it's lower
+                        * edge being placed above the element it should stick to.
+                        */
 
-                    var newsSection = document.getElementById('news');
-                    var gigsSection = document.getElementById('gigs');
-                    var memberSection = document.getElementById('member-section');
-                    var aboutSection = document.getElementById('about');
-                    var mediaSection = document.getElementById('media-section');
-                    var contactSection = document.getElementById('contact');
+                       checkBottomPosition();
 
-                    var navbarHeight = document.getElementById('navbar').getBoundingClientRect().height;
+                       navbarButton.removeClass('active');
+                   } else {
+                       element.addClass('expanded');
+                       expandedNavbarHeight = $('#navbar .container-fluid').height();
+                       element.css('height', String(expandedNavbarHeight) + 'px');
 
-                    switch(item.innerHTML) {
-                        case 'OM VILDE':
-                            window.scrollTo(0, getY(aboutSection) - navbarHeight);
-                            break;
-                        case 'MEDLEMMAR':
-                            window.scrollTo(0, getY(memberSection));
-                            break;
-                        case 'NYHETER':
-                            window.scrollTo(0, getY(newsSection) - navbarHeight);
-                            break;
-                        case 'KONSERTER':
-                            window.scrollTo(0, getY(gigsSection) - navbarHeight);
-                            break;
-                        case 'MEDIA':
-                            window.scrollTo(0, getY(mediaSection) - navbarHeight);
-                            break;
-                        case 'KONTAKT':
-                            window.scrollTo(0, getY(contactSection) - navbarHeight);
-                            break;
-                    }
-                }
+                       /*
+                        * If the navbar is expanded so that its upper edge
+                        * is off screen, the window has to be scrolled up to account
+                        * for this.
+                        */
 
-                for(var i = 0; i < navbarTextElements.length; i++) {
-                    navbarTextElements[i].addEventListener('click', headingClick, false);
-                }
-            }
-        };
-    });
-    
+                       var navbarY = element.offset().top;
+                       if(navbarY < window.scrollY) {
+                           $('html').scrollTop(navbarY);
+                       }
+
+                       navbarButton.addClass('active');
+                   }
+
+               };
+
+               $(window).scroll(navbarPositionListener);
+           }
+       }
+
+   };
+
+   angular.module('vilde')
+       .directive('stickyNavbar', stickyNavbar);
 });
--- app/modules/vilde/references.js
+++ app/modules/vilde/references.js
@@ -12,7 +12,7 @@ define(function() {
         'modules/vilde/services/get-date-service',
         'modules/vilde/services/send-object-service',
         'modules/vilde/directives/gig',
-        'modules/vilde/directives/sticky-navbar2/sticky-navbar2',
+        'modules/vilde/directives/sticky-navbar/sticky-navbar',
         'modules/vilde/directives/youtube',
         'modules/vilde/directives/asynch-iframe'
 
